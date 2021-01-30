f50bc8ae7eacb3df61cfbb52cb217cfe32901b86
anaesperanzalin <anaesperanzalin@users.noreply.github.com>
2021-01-30 15:25:44 -0800

    metrics

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import { getMetricMetaInfo } from './utils/helpers';
 
 export default function App() {
+  
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is Ana´s React Native App!</Text>
+      
+      <MaterialCommunityIcons name="rice" size={80} color="black" />
+      {getMetricMetaInfo('bike').getIcon()}
       <StatusBar style="auto" />
     </View>
   );
