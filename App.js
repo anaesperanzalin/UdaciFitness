cef314003c872c22fb3716730fed5f99b6be237b
anaesperanzalin <anaesperanzalin@users.noreply.github.com>
2021-01-30 19:14:14 -0800

    AddEntry error

--- App.js
+++ App.js
@@ -1,29 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 import { getMetricMetaInfo } from './utils/helpers';
-
-export default function App() {
-  
+import AddEntry from "./components/AddEntry"
 
 
-  return (
-    <View style={styles.container}>
-      <Text>This is Ana´s React Native App!</Text>
-      
-      <MaterialCommunityIcons name="rice" size={80} color="black" />
-      {getMetricMetaInfo('bike').getIcon()}
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render(){
+    return (
+      <View>
+        <AddEntry/>
+
+      </View>
+    )
+  }  
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
